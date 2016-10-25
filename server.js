8df0aca9b89c39893c08996e11ba30918c8c10aa
ko-tori <littletori0912@gmail.com>
2016-10-25 18:36:22 +0000

    removed game and changed editor directory to '/'

--- server.js
+++ server.js
@@ -33,50 +33,6 @@ var oppai = function(path, onstdout, onstderr, onerr) {
 
 app.use(express.static('nso'));
 
-app.get('/', function(req, res) {
-	res.redirect('/nso');
-});
-
-app.get('/editor', function(req, res) {
-	res.sendFile(__dirname + '/nso/editor.html');
-});
-
-app.get('/test/:dir/:file', function(req, res) {
-	res.send(req.params.dir + ', ' + req.params.file);
-});
-
-app.get('/skin/:skin/:file', function(req, res) {
-	var path = __dirname + '/nso/Skins/' + req.params.skin + '/' + req.params.file;
-	if (fs.existsSync(path))
-		res.sendFile(path);
-	else {
-		path = __dirname + '/nso/Skins/Default/' + req.params.file;
-		if (fs.existsSync(path))
-			res.sendFile(path);
-		else
-			res.sendStatus(599);
-	}
-});
-
-app.get('/parsemap/:dir/:file', function(req, res) {
-	var file = "nso/Songs/" + req.params.dir + "/" + req.params.file;
-	parser.parseFile(file, function(err, beatmap) {
-		if (err) console.log(err);
-		res.json(beatmap);
-	});
-});
-
-app.get('/parseskin/:file', function(req, res) {
-	var file = "nso/Skins/" + req.params.file + "/skin.ini";
-	parser.parseSkinFile(file, function(err, skin) {
-		if (err) {
-			console.log(err);
-			res.json(err)
-		}
-		res.json(skin);
-	});
-});
-
 io.on('connection', function(socket) {
 	var delivery = dl.listen(socket);
 	delivery.on('receive.success', function(file) {
