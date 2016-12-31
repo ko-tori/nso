6c3219db03c8d00e041f5160619eadd4d1f8e3cc
ko-tori <littletori0912@gmail.com>
2016-12-31 16:06:40 +0900

    live sync of edits to position, wip

--- server.js
+++ server.js
@@ -62,60 +62,76 @@ app.get("/d/:id", function(req, res) {
 
 var create_room = function(roomID) {
 	var url = "/d/" + roomID;
-	if (!(url in io.nsps)) {
-		var nsp = io.of(url);
-		var room = rooms[url.substring(3)];
-		console.log("created room: " + url);
-		var clients = {};
-		nsp.on('connection', function(socket) {
-			console.log(socket.id + ' joined ' + url);
+	var room = rooms[roomID];
+	Beatmap.ParseFile(`uploads/${room.map}/${room.difficulty}`, function(beatmap) {
+		//console.log(beatmap);
+		if (!(url in io.nsps)) {
+			var nsp = io.of(url);
+			console.log("created room: " + url);
+			var clients = {};
+			nsp.on('connection', function(socket) {
+				console.log(socket.client.id + ' joined ' + url);
 
-			socket.emit('hi', { difficulty: room.difficulty });
+				socket.emit('hi', { difficulty: room.difficulty });
 
-			socket.on('join', function(data) {
-				for (var i in clients) {
-					if (clients.hasOwnProperty(i)) {
-						socket.emit('join', i);
+				socket.on('join', function(data) {
+					for (var i in clients) {
+						if (clients.hasOwnProperty(i)) {
+							socket.emit('join', [i, clients[i]]);
+						}
 					}
-				}
-				socket.broadcast.emit('join', [socket.id, data]);
-				clients[socket.id] = data;
+					socket.broadcast.emit('join', [socket.client.id, data]);
+					clients[socket.client.id] = data;
+				});
+
+				socket.on('msg', function(data) {
+					socket.broadcast.emit('msg', data);
+				});
+
+				socket.on('disconnect', function(data) {
+					delete clients[socket.client.id];
+					socket.broadcast.emit('leave', socket.client.id);
+				});
+
+				var delivery = dl.listen(socket);
+				delivery.on('delivery.connect', function(dlinstance) {
+
+					function send() {
+						dlinstance.send({
+							name: room.map + '.osz',
+							path: path.join('oszcache', room.map)
+						});
+					}
+
+					socket.on('osz', send);
+
+					send();
+
+					// dlinstance.on('send.start', function(filePackage) {
+					// 	console.log("File is being sent to the client.");
+					// });
+
+					// dlinstance.on('send.success', function(file) {
+					// 	console.log('File successfully sent to client!');
+					// });
+
+				});
+
+				socket.on('edit move', function(data) {
+					console.log(data);
+					var obj = beatmap.matchObj(HitObject.parse(data[0]));
+					if(obj) {
+						obj.position.x = data[1];
+						obj.position.y = data[2];
+					}
+					else {
+						//console.log('object not found!');
+					}
+					socket.broadcast.emit('edit move', data);
+				});
 			});
-
-			socket.on('msg', function(data) {
-				socket.broadcast.emit('msg', data);
-			});
-
-			socket.on('disconnect', function(data) {
-				delete clients[socket.id];
-				socket.broadcast.emit('leave', socket.id);
-			});
-
-			var delivery = dl.listen(socket);
-			delivery.on('delivery.connect', function(dlinstance) {
-
-				function send() {
-					dlinstance.send({
-						name: room.map + '.osz',
-						path: path.join('oszcache', room.map)
-					});
-				}
-
-				socket.on('osz', send);
-
-				send();
-
-				// dlinstance.on('send.start', function(filePackage) {
-				// 	console.log("File is being sent to the client.");
-				// });
-
-				// dlinstance.on('send.success', function(file) {
-				// 	console.log('File successfully sent to client!');
-				// });
-
-			});
-		});
-	}
+		}
+	});
 };
 
 var get_room_list = function() {
